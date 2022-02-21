de156cb4677bc5295688ebc1c7a0bb1e1e29e21d
dagmmar <svulpe25@gmail.com>
2022-02-21 18:58:44 +0200

    Solved ex 2

---
+++ Lab 1/Ex2.cpp
@@ -0,0 +1,44 @@
+#include <cstdio> 
+#include <cstring> 
+#define _CRT_SECURE_NO_WARNINGS
+
+FILE * fptr; 
+
+int string_to_number(char s[], int cnt)
+{
+    int nr = 0, p = 1;
+    for(int i = cnt - 1; i >= 0; -- i)
+    {
+        nr = nr + (s[i] - '0') * p; 
+        p *= 10;
+    }
+    return nr;
+}
+
+int main()
+{
+    fptr = fopen("in.txt", "r");
+
+    int cnt = 0, sum = 0;;
+    char ch, s[105];
+    
+    s[0] = '\0';  
+    ch = getc(fptr);
+
+    while(ch != EOF)
+    {
+        if(ch == '\n')
+        {
+            s[cnt] = '\0';
+            sum += string_to_number(s, cnt);
+            cnt = 0;
+        }
+        else 
+            s[cnt ++] = ch; 
+        ch = getc(fptr); 
+    } 
+
+    printf("%d", sum);
+
+    return 0;
+}
